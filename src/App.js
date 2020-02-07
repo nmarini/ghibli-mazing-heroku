aa082b080cff296af8d548f559e62a0ac03acea6
nmarini <nmarini44@gmail.com>
2020-02-07 14:28:43 -0800

    changed App component back to functonal component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import MenuContainer from './components/MenuContainer';
+import ItemsContainer from './components/ItemsContainer';
 
-class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      movie 
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <MenuContainer />
-      </div>
-    )
-  }
-  
+function App() {
+  return (
+    <div className="App">
+      <ItemsContainer />
+    </div>
+  );
 }
 
 export default App;
